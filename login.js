786fb412ccdf40a3f1b93bb7bc587606eb81c361
Jayson Tian <48577877+Jaysontian@users.noreply.github.com>
2021-10-09 23:52:01 -0400

    hi

--- login.js
+++ login.js
@@ -1,3 +1,13 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err));
+    });
+  }
+  
+
 var firebaseConfig = {
     apiKey: "AIzaSyD1YBSA5DcwjK92qsMeiYturKLA1Q2EQdw",
     authDomain: "matcha-82995.firebaseapp.com",
---
+++ serviceworker.js
@@ -0,0 +1,32 @@
+const staticDevCoffee = "dev-coffee-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/base.css",
+  "/daycon.css",
+  "/login.css",
+  "/settings.css",
+  "/topbar.css",
+  "/app.js",
+  "/event.js",
+  "/login.js",
+  "/pages.js",
+  "/serviceworker.js",
+  "/images/logo.png",
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
